d4dde20903deb561716a5c8577cd367bc0a352d7
SHKim9020 <53808062+SHKim9020@users.noreply.github.com>
2023-03-17 00:47:37 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,14 +1,32 @@
-basic.showIcon(IconNames.Ghost)
-pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
-basic.forever(function () {
+function 가변저항LED제어 () {
+    led.plotBarGraph(
+    pins.analogReadPin(AnalogPin.P0),
+    1023
+    )
+    pins.analogWritePin(AnalogPin.P1, pins.map(
+    pins.analogReadPin(AnalogPin.P0),
+    0,
+    1023,
+    0,
+    255
+    ))
+}
+function 버튼동작 () {
     if (pins.digitalReadPin(DigitalPin.P2) == 0) {
         basic.showIcon(IconNames.Yes)
-        pins.digitalWritePin(DigitalPin.P1, 1)
-        basic.pause(500)
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        basic.pause(500)
+        LED동작()
     } else {
-        pins.digitalWritePin(DigitalPin.P1, 0)
         basic.showIcon(IconNames.No)
+        pins.digitalWritePin(DigitalPin.P1, 0)
     }
+}
+function LED동작 () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(500)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    basic.pause(500)
+}
+pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
+basic.forever(function () {
+	
 })
